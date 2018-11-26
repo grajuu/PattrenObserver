af8247c27c6896206c01d2bb54e3b776f123787e
MGR <mardubudi@Govinds-MacBook-Air.local>
2018-11-26 16:07:14 +0530

    Initial commit

---
+++ src/main/java/com/tis/mgr/pattran/observer/PattrenObserverApplication.java
@@ -0,0 +1,14 @@
+package com.tis.mgr.pattran.observer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PattrenObserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PattrenObserverApplication.class, args);
+
+		
+	}
+}
---
+++ src/main/java/com/tis/mgr/pattran/observer/executer/ObserverEvent.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.tis.mgr.pattran.observer.executer;
+
+/**
+ * @author mardubudi
+ *
+ */
+public interface ObserverEvent {
+	
+
+}
